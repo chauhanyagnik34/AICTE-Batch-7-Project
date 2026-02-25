13d06a9575afc9f8d660d3c1cec21ac5940ee260
chauhanyagnik34 <yagnikchauhan34@gmail.com>
2026-02-25 22:55:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+import gdown
+import os
+
+
+file_id = "1BDy2_Lo82Ib1NpB8N0X-73tPBeWPv6kU"
+url = f"https://drive.google.com/uc?id={file_id}"
+output = "similarity.pkl"
+
+if not os.path.exists(output):
+    gdown.download(url, output, quiet=False)
+
+# Now load it
+with open("similarity.pkl", "rb") as f:
+    similarity = pickle.load(f)
+def fetch_poster(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+        params = {
+            "api_key": "8265bd1679663a7ea12ac168da84d2e8",
+            "language": "en-US"
+        }
+
+        response = requests.get(url, params=params, timeout=10)
+        response.raise_for_status()  # Raises HTTPError for bad responses
+
+        data = response.json()
+        poster_path = data.get('poster_path')
+
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
+        else:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+    except requests.exceptions.RequestException as e:
+        print("Error fetching poster:", e)
+        return "https://via.placeholder.com/500x750?text=Error"
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies,recommended_movies_posters
+
+movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+movies = pd.DataFrame.from_dict(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+'Select a movie',
+movies['title'].values)
+
+if st.button('Recommend'):
+    names,posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
+
+
